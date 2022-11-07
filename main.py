c47e01eedef6f4e34349cc5f73aa8879ba1d38ce
RohanJoshi28 <rjoshi6@gmail.com>
2022-11-07 08:36:50 -0500

    add first html template

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
