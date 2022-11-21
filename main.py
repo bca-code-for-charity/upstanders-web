d65c45dc83b0fffff12c92de75e6df59d48f5d73
RohanJoshi28 <rjoshi6@gmail.com>
2022-11-20 21:22:08 -0500

    toast's calendar page

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template("index.html")
+
+@app.route('/calendar')
+def calendar():
+    return render_template("calendar.html")
